8b9c1c8d067b1948af0d5f914a78f0179de7533f
Vladimir <vladville@gmail.com>
2025-04-01 00:17:03 +1000

    Another test added. Coverage 94% reached

--- 03_control/02_task/src/test/kotlin/MainKtTest.kt
+++ 03_control/02_task/src/test/kotlin/MainKtTest.kt
@@ -15,6 +15,17 @@ class MainKtTest {
     }
 
     @Test
+    fun calculateCommissionMCZero() {
+        val cardType = "Mastercard"
+        val prevTrans = 2000
+        val curTrans = 70_000
+
+        val result = calculateCommission(cardType, prevTrans, curTrans)
+
+        assertEquals(0, result)
+    }
+
+    @Test
     fun calculateCommissionMir() {
         val cardType = "Мир"
         val prevTrans = 0
@@ -34,5 +45,60 @@ class MainKtTest {
         val result = calculateCommission(cardType, prevTrans, curTrans)
 
         assertEquals(600, result)
+    }
+
+    @Test
+    fun calculateCommissionOther() {
+        val cardType = "Corona"
+        val prevTrans = 0
+        val curTrans = 80_000
+
+        val result = calculateCommission(cardType, prevTrans, curTrans)
+
+        assertEquals(-1, result)
+    }
+
+    @Test
+    fun calculateCommissionBigCurTrans() {
+        val cardType = "Mastercard"
+        val prevTrans = 0
+        val curTrans = 180_000
+
+        val result = calculateCommission(cardType, prevTrans, curTrans)
+
+        assertEquals(-1, result)
+    }
+
+    @Test
+    fun calculateCommissionReachLimit() {
+        val cardType = "Mastercard"
+        val prevTrans = 480_000
+        val curTrans = 180_000
+
+        val result = calculateCommission(cardType, prevTrans, curTrans)
+
+        assertEquals(-1, result)
+    }
+
+    @Test
+    fun calculateCommissionWithPrevTrans() {
+        val cardType = "Mastercard"
+        val prevTrans = 76_000
+        val curTrans = 20_000
+
+        val result = calculateCommission(cardType, prevTrans, curTrans)
+
+        assertEquals(140, result)
+    }
+
+    @Test
+    fun calculateCommissionVisaMinCommission() {
+        val cardType = "Visa"
+        val prevTrans = 0
+        val curTrans = 100
+
+        val result = calculateCommission(cardType, prevTrans, curTrans)
+
+        assertEquals(35, result)
     }
 }
