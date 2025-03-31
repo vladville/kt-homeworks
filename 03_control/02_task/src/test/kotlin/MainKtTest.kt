f41b50f325ce87c26685e91cc600d82dcc172cfa
Vladimir <vladville@gmail.com>
2025-03-31 23:39:53 +1000

    AutoTest added

---
+++ 03_control/02_task/src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,38 @@
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class MainKtTest {
+
+    @Test
+    fun calculateCommissionMC() {
+        val cardType = "Mastercard"
+        val prevTrans = 0
+        val curTrans = 78_000
+
+        val result = calculateCommission(cardType, prevTrans, curTrans)
+
+        assertEquals(38, result)
+    }
+
+    @Test
+    fun calculateCommissionMir() {
+        val cardType = "Мир"
+        val prevTrans = 0
+        val curTrans = 100_000
+
+        val result = calculateCommission(cardType, prevTrans, curTrans)
+
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun calculateCommissionVisa() {
+        val cardType = "Visa"
+        val prevTrans = 0
+        val curTrans = 80_000
+
+        val result = calculateCommission(cardType, prevTrans, curTrans)
+
+        assertEquals(600, result)
+    }
+}
